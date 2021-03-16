fe492d953a25b574bce1a79e1b5766da49b8719b
Adely Perez <adelyalvla@gmail.com>
2021-03-15 21:56:34 -0600

    Subiendo programa de Ordenes

---
+++ Ejercicios/9-notas-escolares/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _9_notas_escolares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Escuela escuela = new Escuela(1, "Happy");
+
+            Alumno alumno = new Alumno(5, "Pablo", "77777");
+            alumno.Direccion = "SPS";
+            alumno.NoCarnet = "12345";
+
+            escuela.matricularAlumno(alumno);            
+            escuela.mostrarAlumnos();
+        }
+    }
+}
